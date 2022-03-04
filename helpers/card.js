f000be939923de39471db39688a516e7607b2a44
Justin Diaz <justin.s.diaz@gmail.com>
2022-03-03 22:51:30 -0500

    create card object

---
+++ helpers/card.js
@@ -0,0 +1,11 @@
+class card {
+  constructor(type, name, effect, characterClass, attack, defense, pitch) {
+    this.type = type;
+    this.name = name;
+    this.effect = effect;
+    this.characterClass = characterClass;
+    this.attack = attack;
+    this.defense = defense;
+    this.pitch = pitch;
+  }
+}
