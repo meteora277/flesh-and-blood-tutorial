9537598a2b1a10b1bbc5370c51a6f643280bf697
Justin Diaz <justin.s.diaz@gmail.com>
2022-03-03 23:28:51 -0500

    create class loadout

---
+++ helpers/loadOut.js
@@ -0,0 +1,11 @@
+class loadOut {
+  constructor(head, chest, arms, legs, weapon1, weapon2, hero) {
+    this.head = head;
+    this.chest = chest;
+    this.arms = arms;
+    this.legs = legs;
+    this.weapon1 = weapon1;
+    this.weapon2 = weapon2;
+    this.hero = hero;
+  }
+}
