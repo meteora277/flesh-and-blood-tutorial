5a7831bad5aa595012a85a93e5a17adbef3af4b2
Justin Diaz <justin.s.diaz@gmail.com>
2022-03-04 17:38:39 -0500

    change game to use player class

--- helpers/game.js
+++ helpers/game.js
@@ -1,8 +1,14 @@
 import { player } from "./player";
 
-class game {
-  player1 = new player("Justin");
-  get player1() {
+export default class game {
+  constructor(name1, name2) {
+    this.player1 = new player(name1);
+    this.player2 = new player(name2);
+  }
+  getPlayer1() {
     return this.player1.name;
+  }
+  setPlayer1(name) {
+    this.player1 = name;
   }
 }
