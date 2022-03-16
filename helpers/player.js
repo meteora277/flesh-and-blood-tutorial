07c6ed102c13d011e62b7ab4d0ecfdef937b3fa3
Justin Diaz <justin.s.diaz@gmail.com>
2022-03-15 21:20:48 -0400

    move hero hp and intellect into player

--- helpers/player.js
+++ helpers/player.js
@@ -1,5 +1,16 @@
 export class player {
-  constructor(name) {
+  constructor(name, hp, intellect) {
     this.name = name || "";
+    this.hp = hp;
+    this.intellect = intellect;
+  }
+  changeHp(num) {
+    this.hp += num;
+  }
+  changeIntellect(num) {
+    this.intellect += num;
+  }
+  getHp() {
+    return this.hp;
   }
 }
