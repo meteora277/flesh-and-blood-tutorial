e6a84e28673160bbaa7251a5c0f54b4e3ccec142
Justin Diaz <justin.s.diaz@gmail.com>
2022-03-15 21:19:55 -0400

    create isGameOver method

--- helpers/game.js
+++ helpers/game.js
@@ -2,13 +2,14 @@ import { player } from "./player";
 
 export default class game {
   constructor(name1, name2) {
-    this.player1 = new player(name1);
-    this.player2 = new player(name2);
+    this.player1 = new player(name1, 20, 4);
+    this.player2 = new player(name2, 20, 4);
   }
-  getPlayer1() {
-    return this.player1.name;
-  }
-  setPlayer1(name) {
-    this.player1 = name;
+  isGameOver() {
+    if (this.player1.getHp() === 0 || this.player2.getHp() === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
